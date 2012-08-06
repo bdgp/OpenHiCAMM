b9e06d97eac76c3420aa699b59744cf33046e3a8
Ben Booth <benwbooth@gmail.com>
2012-08-06 11:03:41 -0700

    add modules.txt file rename slideworkflowdialog to workflowdialog add stub for workflow status dialog remove xml generator

---
+++ MMSlideImage/src/org/bdgp/MMSlide/WorkflowStatusDialog.java
@@ -0,0 +1,10 @@
+package org.bdgp.MMSlide;
+
+import javax.swing.JDialog;
+
+@SuppressWarnings("serial")
+public class WorkflowStatusDialog extends JDialog {
+    public WorkflowStatusDialog() {
+    }
+
+}
