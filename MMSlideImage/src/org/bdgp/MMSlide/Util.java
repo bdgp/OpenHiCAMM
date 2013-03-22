c406e7b823ea60a07f98021f62d9e176e02e3fa4
Ben Booth <benwbooth@gmail.com>
2013-03-22 14:01:32 -0700

    add more utility routines

--- MMSlideImage/src/org/bdgp/MMSlide/Util.java
+++ MMSlideImage/src/org/bdgp/MMSlide/Util.java
@@ -2,6 +2,7 @@ package org.bdgp.MMSlide;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Util {
     /**
@@ -19,10 +20,20 @@ public class Util {
         public HashMap<K,V> and(K k, V v) { this.put(k, v); return this; }
         public HashMap<K,V> o(K k, V v) { this.put(k, v); return this; }
         public HashMap<K,V> _(K k, V v) { this.put(k, v); return this; }
+        // in-place merge
+        public Map<K,V> merge(Map<K,V> map) {
+            this.putAll(map);
+            return this;
+        }
     }
     // static methods for creating various HashMaps
     public static <K,V> HashMap<K,V> map(K k, V v) {
         return new HashMap<K,V>().with(k, v);
+    }
+    public static <K,V> HashMap<K,V> map(Map<K,V> m) {
+        HashMap<K,V> map = new HashMap<K,V>();
+        map.putAll(m);
+        return map;
     }
     public static HashMap<String,Object> set(String k, Object v) {
         return new HashMap<String,Object>().with(k, v);
@@ -33,6 +44,21 @@ public class Util {
     public static HashMap<String,String> attr(String k, String v) {
         return new HashMap<String,String>().with(k, v);
     }
+    
+    @SuppressWarnings("serial")
+    public static class ArrayList<V> extends java.util.ArrayList<V> {
+        public ArrayList() {
+            super();
+        }
+        public ArrayList<V> concat(List<V> l) {
+            this.addAll(l);
+            return this;
+        }
+        public ArrayList<V> push(V ... values) {
+            this.addAll(Arrays.asList(values));
+            return this;
+        }
+    }
         
     /**
      * Simple static function for list literals.
@@ -42,5 +68,9 @@ public class Util {
     public static <V> List<V> list(V ... values) {
         return Arrays.asList(values);
     }
-    
+    public static <V> List<V> list(List<V> l) {
+        List<V> list = new ArrayList<V>();
+        list.addAll(l);
+        return list;
+    }
 }
