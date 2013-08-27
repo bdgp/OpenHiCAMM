b149750f445a1a07ea9f89fc044d4f27ec56e9ee
Ben Booth <benwbooth@gmail.com>
2013-08-27 16:11:20 -0700

    add constructor for specifying pool id

--- MMSlideImage/src/org/bdgp/MMSlide/DB/Pool.java
+++ MMSlideImage/src/org/bdgp/MMSlide/DB/Pool.java
@@ -7,7 +7,11 @@ import com.j256.ormlite.table.DatabaseTable;
 public class Pool {
     @DatabaseField(generatedId=true, canBeNull=false)
     private int id;
+
     public Pool() { }
+    public Pool(int id) { 
+        this.id = id;
+    }
     public int getId() { return this.id; }
     public String getName() { return String.format("P%05d", this.id); }
 }
