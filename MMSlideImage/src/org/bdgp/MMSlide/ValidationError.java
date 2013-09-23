6be16689b7ac42b7c4cac8a466a49b7b6e9a8186
Ben Booth <benwbooth@gmail.com>
2013-09-23 15:02:11 -0700

    add ValidationError class for storing info on validation errors

---
+++ MMSlideImage/src/org/bdgp/MMSlide/ValidationError.java
@@ -0,0 +1,18 @@
+package org.bdgp.MMSlide;
+
+public class ValidationError {
+    private String moduleId;
+    private String message;
+    
+    public ValidationError(String moduleId, String message) {
+        this.moduleId = moduleId;
+        this.message = message;
+    }
+    
+    public String getModuleId() {
+        return this.moduleId;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+}
