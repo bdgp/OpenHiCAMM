9276bf2b53848886140995d221a3596bf31c5c56
Ben Booth <benwbooth@gmail.com>
2013-09-23 15:03:16 -0700

    add Comparable interface to PoolSlide

--- MMSlideImage/src/org/bdgp/MMSlide/DB/PoolSlide.java
+++ MMSlideImage/src/org/bdgp/MMSlide/DB/PoolSlide.java
@@ -4,7 +4,7 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable
-public class PoolSlide {
+public class PoolSlide implements Comparable<PoolSlide> {
     @DatabaseField(canBeNull=false, uniqueCombo=true)
     private int poolId;
     @DatabaseField(canBeNull=false, uniqueCombo=true)
@@ -15,9 +15,10 @@ public class PoolSlide {
     private int slideId;
     
     public PoolSlide() {}
-    public PoolSlide(int cartridgeId, int slideNumber, int slideId) {
-        this.cartridgePosition = cartridgeId;
-        this.slidePosition = slideNumber;
+    public PoolSlide(int poolId, int cartridgePosition, int slidePosition, int slideId) {
+        this.poolId = poolId;
+        this.cartridgePosition = cartridgePosition;
+        this.slidePosition = slidePosition;
         this.slideId = slideId;
     }
 
@@ -25,4 +26,37 @@ public class PoolSlide {
     public int getCartridgePosition() { return cartridgePosition; }
     public int getSlidePosition() { return slidePosition; }
     public int getSlideId() { return slideId; }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + cartridgePosition;
+        result = prime * result + slidePosition;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PoolSlide other = (PoolSlide) obj;
+        if (cartridgePosition != other.cartridgePosition)
+            return false;
+        if (slidePosition != other.slidePosition)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(PoolSlide o) {
+        int result = this.cartridgePosition-o.cartridgePosition;
+        if (result == 0) {
+            result = this.slidePosition-o.slidePosition;
+        }
+        return result;
+    }
 }
