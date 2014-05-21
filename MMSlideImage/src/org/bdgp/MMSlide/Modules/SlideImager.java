0802fd78801f8101a22493c8e1c9cec4afd20b3f
Ben Booth <benwbooth@gmail.com>
2014-05-21 12:09:36 -0700

    add implementations for loading/saving config, and running the multi D acquisition dialog

--- MMSlideImage/src/org/bdgp/MMSlide/Modules/SlideImager.java
+++ MMSlideImage/src/org/bdgp/MMSlide/Modules/SlideImager.java
@@ -1,15 +1,22 @@
 package org.bdgp.MMSlide.Modules;
 
 import java.awt.Component;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
 
 import javax.swing.JPanel;
 
 import org.bdgp.MMSlide.Logger;
-import org.bdgp.MMSlide.Util;
 import org.bdgp.MMSlide.ValidationError;
 import org.bdgp.MMSlide.WorkflowRunner;
 import org.bdgp.MMSlide.DB.Config;
@@ -19,10 +26,9 @@ import org.bdgp.MMSlide.DB.TaskDispatch;
 import org.bdgp.MMSlide.DB.WorkflowModule;
 import org.bdgp.MMSlide.Modules.Interfaces.Configuration;
 import org.bdgp.MMSlide.Modules.Interfaces.Module;
-import org.micromanager.PositionListDlg;
-import org.micromanager.api.PositionList;
+import org.micromanager.AcqControlDlg;
 import org.micromanager.api.ScriptInterface;
-import org.micromanager.utils.MMSerializationException;
+import org.micromanager.utils.MMScriptException;
 
 import static org.bdgp.MMSlide.Util.where;
 import static org.bdgp.MMSlide.Util.map;
@@ -30,16 +36,40 @@ import static org.bdgp.MMSlide.Util.map;
 public class SlideImager implements Module {
     WorkflowRunner workflowRunner;
     String moduleId;
+    AcqControlDlg acqControlDlg;
+    ScriptInterface script;
 
     @Override
     public void initialize(WorkflowRunner workflowRunner, String moduleId) {
         this.workflowRunner = workflowRunner;
         this.moduleId = moduleId;
+        this.script = workflowRunner.getMMSlide().getApp();
+        if (script != null) {
+            this.acqControlDlg = script.getAcqDlg();
+        }
     }
 
     @Override
     public Status run(Task task, Map<String,Config> config, Logger logger) {
-        Util.sleep();
+        if (acqControlDlg != null) {
+            String rootDir = new File(workflowRunner.getWorkflowDirectory(),
+            		task.getStorageLocation()).getParent();
+            String acqName = new File(workflowRunner.getWorkflowDirectory(),
+            		task.getStorageLocation()).getName();
+        	String returnAcqName = acqControlDlg.runAcquisition(acqName, rootDir);
+        	if (returnAcqName == null) {
+        		throw new RuntimeException("acqControlDlg.runAcquisition returned null acquisition name");
+        	}
+        	logger.info("Started acquisition to directory "+rootDir);
+        	while (acqControlDlg.isAcquisitionRunning()) {
+        		 try { Thread.sleep(1); } 
+        		 catch (InterruptedException e) { 
+        			 logger.info("Thread was interrupted.");
+        		 } 
+        	}
+        	logger.info("Acquisition "+acqName+" finished.");
+            return Status.SUCCESS;
+        }
         return Status.SUCCESS;
     }
 
@@ -56,16 +86,28 @@ public class SlideImager implements Module {
     @Override
     public Configuration configure() {
         return new Configuration() {
-            PositionListDlg posListDlg;
-            PositionList posList;
             @Override
             public Config[] retrieve() {
             	List<Config> configs = new ArrayList<Config>();
-            	try {
-            		if (posList != null) {
-                        configs.add(new Config(moduleId, "positionList", posList.serialize()));
-            		}
-				} catch (MMSerializationException e) {throw new RuntimeException(e);}
+                if (acqControlDlg != null) {
+                    try {
+                    	// Use reflection to get at the acqPrefs_ private member.
+                    	// Ugly, but it's the only way I could figure out to save
+                    	// the prefs without popping up a Save As dialog.
+                        Field f = acqControlDlg.getClass().getDeclaredField("acqPrefs_");
+                        f.setAccessible(true);
+                        Preferences acqPrefs = (Preferences) f.get(acqControlDlg);
+                        OutputStream prefs = new ByteArrayOutputStream();
+                        acqPrefs.exportNode(prefs);
+                        configs.add(new Config(moduleId, "acqDlgSettings", prefs.toString()));
+                    } 
+                    catch (NoSuchFieldException e) {throw new RuntimeException(e);} 
+                    catch (SecurityException e) {throw new RuntimeException(e);} 
+                    catch (IllegalArgumentException e) {throw new RuntimeException(e);} 
+                    catch (IllegalAccessException e) {throw new RuntimeException(e);} 
+                    catch (IOException e) {throw new RuntimeException(e);} 
+                    catch (BackingStoreException e) {throw new RuntimeException(e);}
+                }
                 return configs.toArray(new Config[0]);
             }
             @Override
@@ -74,18 +116,25 @@ public class SlideImager implements Module {
                 for (Config config : configs) {
                     conf.put(config.getKey(), config);
                 }
-                // Positions (what to image: entire slide, previously defined positions)
-                ScriptInterface script = workflowRunner.getMMSlide().getApp();
-                if (script != null) {
-                    posListDlg = script.getXYPosListDlg();
-                    posList = new PositionList();
-                    if (conf.containsKey("positionList")) {
-                    	try {
-							posList.restore(conf.get("positionList").getValue());
-						} catch (MMSerializationException e) {throw new RuntimeException(e);}
-                    }
-                    posListDlg.setPositionList(posList);
-                    return posListDlg;
+                if (acqControlDlg != null) {
+                    try {
+                    	// AcqControlDlg has a public method for loading 
+                    	// preferences from a file, so we can use a temp file 
+                    	// here to store the preferences, then load them, and 
+                    	// delete the temp file. Still ugly, but not as ugly 
+                    	// as grabbing a private field.
+                    	Config c = conf.get("acqDlgSettings");
+						File tempFile = File.createTempFile("mmslideimageprefs_",".prefs");
+						PrintWriter out = new PrintWriter(tempFile.getPath());
+						out.print(c.getValue());
+						out.close();
+						acqControlDlg.loadAcqSettingsFromFile(tempFile.getPath());
+						tempFile.delete();
+					} 
+                    catch (MMScriptException e) {throw new RuntimeException(e);}
+					catch (IOException e) {throw new RuntimeException(e);}
+
+                    return acqControlDlg;
                 }
                 return new JPanel();
             }
