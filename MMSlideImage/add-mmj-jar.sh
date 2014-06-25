5fa535c461add1458a3aed279267add4b4ed8c84
Ben Booth <benwbooth@gmail.com>
2014-06-24 17:46:29 -0700

    add updated MMJ_.jar fix add-mmj-jar.sh script

--- MMSlideImage/add-mmj-jar.sh
+++ MMSlideImage/add-mmj-jar.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 test -z "$1" && echo "Usage: $0 micromanager-dir" >&2 && exit 1
-mvn install:install-file -Dfile="$1"/mmstudio/MMJ_.jar -DgroupId=MicroManager -DartifactId=MMJ_ -Dversion="$(cat "$1"/version.txt)" -Dpackaging=jar
+mvn install:install-file -Dfile="$1"/mmstudio/MMJ_.jar -DgroupId=MicroManager -DartifactId=MMJ_ -Dversion="$(cat "$1"/version.txt)" -Dpackaging=jar -DlocalRepositoryPath="$PWD"/repository
