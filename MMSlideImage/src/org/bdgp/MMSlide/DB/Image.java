f8ac5947be5bfcf6dd13ac6d92a72a4864b24289
Ben Booth <benwbooth@gmail.com>
2014-06-30 14:39:52 -0700

    store image tags in Image records

--- MMSlideImage/src/org/bdgp/MMSlide/DB/Image.java
+++ MMSlideImage/src/org/bdgp/MMSlide/DB/Image.java
@@ -1,5 +1,8 @@
 package org.bdgp.MMSlide.DB;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
@@ -10,6 +13,7 @@ public class Image {
     @DatabaseField(canBeNull=false) private int slideId;
     @DatabaseField(canBeNull=false,dataType=DataType.LONG_STRING) private String path;
     @DatabaseField(canBeNull=false) private int slidePosId;
+    @DatabaseField() private String tags;
     
     public Image() {}
     public Image (int slideId, int slidePosId, String path) {
@@ -17,8 +21,26 @@ public class Image {
         this.slidePosId = slidePosId;
         this.path = path;
     }
+    public Image (int slideId, int slidePosId, String path, String tags) {
+        this.slideId = slideId;
+        this.slidePosId = slidePosId;
+        this.path = path;
+        this.tags = tags;
+    }
+    public Image (int slideId, int slidePosId, String path, JSONObject tags) {
+        this.slideId = slideId;
+        this.slidePosId = slidePosId;
+        this.path = path;
+        this.tags = tags.toString();
+    }
+    public int getId() {return this.id;}
     public String getName() {return String.format("I%05d", this.id);}
     public int getSlideId() {return this.slideId;}
     public int getSlidePosId() {return this.slidePosId;}
     public String getPath() {return this.path;}
+    public JSONObject getTags() {
+    	try { return new JSONObject(this.tags); } 
+    	catch (JSONException e) { throw new RuntimeException(e); }
+    }
+    public String getTagsAsString() { return this.tags; }
 }
