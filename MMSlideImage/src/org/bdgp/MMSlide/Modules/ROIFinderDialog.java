c035a9e55ca5a25a971767c549b10914e4347781
Ben Booth <benwbooth@gmail.com>
2014-06-30 16:41:26 -0700

    Config UIs should extend JPanel

--- MMSlideImage/src/org/bdgp/MMSlide/Modules/ROIFinderDialog.java
+++ MMSlideImage/src/org/bdgp/MMSlide/Modules/ROIFinderDialog.java
@@ -1,25 +1,22 @@
 package org.bdgp.MMSlide.Modules;
 
-import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
-public class ROIFinderDialog extends JTabbedPane {
+public class ROIFinderDialog extends JPanel {
 	JTextField posListName;
 	public ROIFinderDialog() {
 		
-		JPanel panel = new JPanel();
-		addTab("New tab", null, panel, null);
-		panel.setLayout(new MigLayout("", "[grow]", "[][]"));
+		this.setLayout(new MigLayout("", "[grow]", "[][]"));
 		
 		JLabel lblEnterPositionList = new JLabel("Enter Name for Saved Position List");
-		panel.add(lblEnterPositionList, "cell 0 0");
+		this.add(lblEnterPositionList, "cell 0 0");
 		
 		posListName = new JTextField();
-		panel.add(posListName, "cell 0 1,growx");
+		this.add(posListName, "cell 0 1,growx");
 		posListName.setColumns(10);
 	}
 
--- MMSlideImage/src/org/bdgp/MMSlide/Modules/SlideImagerDialog.java
+++ MMSlideImage/src/org/bdgp/MMSlide/Modules/SlideImagerDialog.java
@@ -1,6 +1,5 @@
 package org.bdgp.MMSlide.Modules;
 
-import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
@@ -16,14 +15,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
-public class SlideImagerDialog extends JTabbedPane {
+public class SlideImagerDialog extends JPanel {
 	JTextField acqSettingsText;
 	JTextField posListText;
 	JTextField posListName;
 	public SlideImagerDialog(final AcqControlDlg acqControlDlg) {
-		JPanel panel = new JPanel();
-		addTab("New tab", null, panel, null);
-		panel.setLayout(new MigLayout("", "[grow]", "[][][][][][][]"));
+		this.setLayout(new MigLayout("", "[grow]", "[][][][][][][]"));
 		
 		JButton btnShowAcquisitionDialog = new JButton("Show Acquisition Dialog");
 		if (acqControlDlg == null) {
@@ -38,18 +35,18 @@ public class SlideImagerDialog extends JTabbedPane {
 				}
 			}
 		});
-		panel.add(btnShowAcquisitionDialog, "cell 0 0");
+		this.add(btnShowAcquisitionDialog, "cell 0 0");
 		
 		JLabel lblSelectMultid = new JLabel("Load Acqusition Settings File");
-		panel.add(lblSelectMultid, "cell 0 1");
+		this.add(lblSelectMultid, "cell 0 1");
 		
 		acqSettingsText = new JTextField();
 		acqSettingsText.setEditable(false);
-		panel.add(acqSettingsText, "flowx,cell 0 2,growx");
+		this.add(acqSettingsText, "flowx,cell 0 2,growx");
 		acqSettingsText.setColumns(10);
 		
 		JLabel lblLoad = new JLabel("Load Position List From File");
-		panel.add(lblLoad, "cell 0 3");
+		this.add(lblLoad, "cell 0 3");
 		
         final JFileChooser acqSettingsChooser = new JFileChooser();
         acqSettingsChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
@@ -61,11 +58,11 @@ public class SlideImagerDialog extends JTabbedPane {
                 }
 			}
 		});
-		panel.add(btnLoadAcqSettings, "cell 0 2");
+		this.add(btnLoadAcqSettings, "cell 0 2");
 		
 		posListText = new JTextField();
 		posListText.setEditable(false);
-		panel.add(posListText, "flowx,cell 0 4,growx");
+		this.add(posListText, "flowx,cell 0 4,growx");
 		posListText.setColumns(10);
 		
 		final JFileChooser posListChooser = new JFileChooser();
@@ -78,13 +75,13 @@ public class SlideImagerDialog extends JTabbedPane {
                 }
 			}
 		});
-		panel.add(btnLoadPosList, "cell 0 4");
+		this.add(btnLoadPosList, "cell 0 4");
 		
 		JLabel lblPositionListDb = new JLabel("Or Enter Position List DB Name");
-		panel.add(lblPositionListDb, "cell 0 5");
+		this.add(lblPositionListDb, "cell 0 5");
 		
 		posListName = new JTextField();
-		panel.add(posListName, "cell 0 6,growx");
+		this.add(posListName, "cell 0 6,growx");
 		posListName.setColumns(10);
 	}
 }
