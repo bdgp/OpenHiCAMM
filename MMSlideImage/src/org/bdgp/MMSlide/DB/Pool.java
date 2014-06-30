9a6585b39ad8dbbc207135942d1e5bfb96a3355a
Ben Booth <benwbooth@gmail.com>
2014-06-30 14:40:10 -0700

    add function to convert from pool name to ID

--- MMSlideImage/src/org/bdgp/MMSlide/DB/Pool.java
+++ MMSlideImage/src/org/bdgp/MMSlide/DB/Pool.java
@@ -1,5 +1,8 @@
 package org.bdgp.MMSlide.DB;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
@@ -14,4 +17,13 @@ public class Pool {
     }
     public int getId() { return this.id; }
     public String getName() { return String.format("P%05d", this.id); }
+
+	public static Integer name2id(String poolName) {
+        Pattern pattern = Pattern.compile("^P([0-9]+)$");
+        Matcher matcher = pattern.matcher(poolName);
+        if (matcher.find()) {
+            return new Integer(matcher.group(1));
+        }
+		return null;
+	}
 }
