6eaf90dfed3272d6e2b48dae1515491dfbde9255
Ben Booth <benwbooth@gmail.com>
2014-07-11 16:48:47 -0700

    add default constructor

--- MMSlideImage/src/org/bdgp/MMSlide/DB/SlidePos.java
+++ MMSlideImage/src/org/bdgp/MMSlide/DB/SlidePos.java
@@ -14,6 +14,7 @@ public class SlidePos {
     @DatabaseField
     private int roiId;
 
+    public SlidePos() { }
     public SlidePos(int slidePosListId, int slidePosListIndex) {
         this.slidePosListId = slidePosListId;
         this.slidePosListIndex = slidePosListIndex;
