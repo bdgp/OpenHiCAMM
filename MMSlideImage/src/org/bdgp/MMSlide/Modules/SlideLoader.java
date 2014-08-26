7c044eb8f11fbe2a805ba2c37b8dbec01651e377
Ben Booth <benwbooth@gmail.com>
2014-08-26 13:14:04 -0700

    finish implementing the slide loader code

--- MMSlideImage/src/org/bdgp/MMSlide/Modules/SlideLoader.java
+++ MMSlideImage/src/org/bdgp/MMSlide/Modules/SlideLoader.java
@@ -3,6 +3,8 @@ package org.bdgp.MMSlide.Modules;
 import java.awt.Component;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,60 +34,103 @@ import static org.bdgp.MMSlide.Util.where;
 public class SlideLoader implements Module {
     WorkflowRunner workflowRunner;
     String moduleId;
+    SlideLoaderAPI slideLoader;
+    PoolSlide currentSlide;
 
     @Override
     public void initialize(WorkflowRunner workflowRunner, String moduleId) {
         this.workflowRunner = workflowRunner;
         this.moduleId = moduleId;
+        this.slideLoader = new SlideLoaderAPI();
     }
 
 	@Override
-	public Status run(Task task, Map<String, Config> config, Logger logger) {
-        int[] retVal = {0};
-        SlideLoaderAPI sl = new SlideLoaderAPI();
-        Config deviceConfig = config.getOrDefault("device", null);
+	public synchronized Status run(Task task, Map<String, Config> config, Logger logger) {
+		// get the device path
+        Config deviceConfig = config.get("device");
         String device = deviceConfig != null && !deviceConfig.getValue().isEmpty()? 
         		deviceConfig.getValue() : "/dev/tty.usbserial-FTEKUITV";
-        sl.Connect(device, retVal);
-        sl.get_Status(retVal);
-        reportStatus(retVal[0], logger);
+        		
+        // get the PoolSlide ID for this Slide
+        Config poolSlideIdConf = config.get("poolSlideId");
+        Integer poolSlideId = poolSlideIdConf != null? new Integer(poolSlideIdConf.getValue()) : null;
+        		
+        Dao<PoolSlide> poolSlideDao = workflowRunner.getInstanceDb().table(PoolSlide.class);
+        PoolSlide thisSlide = poolSlideId != null? poolSlideDao.selectOneOrDie(where("id",poolSlideId)) : null;
+
+        // get a sorted list of all the Task records for this module
+        Dao<Task> taskDao = workflowRunner.getInstanceDb().table(Task.class);
+        List<Task> tasks = taskDao.select(where("moduleId", this.moduleId));
+        Collections.sort(tasks, new Comparator<Task>() {
+			@Override public int compare(Task o1, Task o2) {
+				return o1.getId()-o2.getId();
+			}});
+
+        // make a note of which Tasks are the first, last, and current.
+        Task firstTask = tasks.get(0);
+        Task lastTask = tasks.get(tasks.size()-1);
+
+        int[] retVal = {0};
+        boolean[] slidePresent = {false};
+        Status status = Status.SUCCESS;
+
+        // Initialize the slide loader if this is the first task
+        if (task.getId() == firstTask.getId()) {
+            slideLoader.Connect(device, retVal);
+            slideLoader.get_Status(retVal);
+            reportStatus(retVal[0], logger);
+            
+            // initialize Prior slide loader.....will take about 20 sec.
+            slideLoader.Initialise(retVal);
+            waitForSlideLoader();
+
+            slideLoader.get_CassettesFitted(retVal);
+            slideLoader.ScanCassette(1, retVal);
+            waitForSlideLoader();
+        }
         
-        // initialize Prior slide loader.....will take about 20 sec.
-        sl.Initialise(retVal);
-        do {
-            try { Thread.sleep(1000); } catch (InterruptedException e) { }
-            sl.get_Status(retVal);
-            //reportStatus(retVal[0], logger);
-        } while (((retVal[0] & getSTATE_STATEMASK()) != getSTATE_IDLE()) && !((retVal[0] & getLOADER_ERROR()) != 0));
+        // Put the previous slide back 
+        if (this.currentSlide != null) {
+            slideLoader.MoveFromStage(
+            		this.currentSlide.getCartridgePosition(), 
+            		this.currentSlide.getSlidePosition(), 
+            		retVal);
+            waitForSlideLoader();
+            this.currentSlide = null;
+        }
 
-        sl.get_CassettesFitted(retVal);
-        sl.ScanCassette(1, retVal);
-        do {
-            sl.get_Status(retVal);
-            //reportStatus(retVal[0], logger);
-        } while (((retVal[0] & getSTATE_STATEMASK()) != getSTATE_IDLE()) && !((retVal[0] & getLOADER_ERROR()) != 0));
-
-        for(int slide = 1; slide < 32; ++slide) {
-            boolean[] slidePresent = {false};
-            sl.get_SlideFitted(1, slide, slidePresent);
-            if(slidePresent[0]) {
-                sl.MoveToStage(1, slide, retVal);
-                do {
-                    try { Thread.sleep(1000); } catch (InterruptedException e) { }
-                    sl.get_Status(retVal);
-                    //reportStatus(retVal[0], logger);
-                } while (((retVal[0] & getSTATE_STATEMASK()) != getSTATE_IDLE()) && !((retVal[0] & getLOADER_ERROR()) != 0));
-
-                sl.MoveFromStage(1, slide, retVal);
-                do {
-                    try { Thread.sleep(1000); } catch (InterruptedException e) { }
-                    sl.get_Status(retVal);
-                    //reportStatus(retVal[0], logger);
-                } while (((retVal[0] & getSTATE_STATEMASK()) != getSTATE_IDLE()) && !((retVal[0] & getLOADER_ERROR()) != 0));
+        // Load the next slide
+        if (thisSlide != null) {
+            // Make sure the slide is present
+            slideLoader.get_SlideFitted(thisSlide.getCartridgePosition(), thisSlide.getSlidePosition(), slidePresent);
+            if (slidePresent[0]) {
+                slideLoader.MoveToStage(thisSlide.getCartridgePosition(), thisSlide.getSlidePosition(), retVal);
+                waitForSlideLoader();
+                this.currentSlide = thisSlide;
+            }
+            // If the slide is missing, mark this task as fail
+            else {
+                logger.severe("Slide at cartridge "+thisSlide.getCartridgePosition()+
+                        ", slide position "+thisSlide.getSlidePosition()+" is missing!");
+                status = Status.FAIL;
             }
         }
-        sl.DisConnect(retVal);
-        return Status.SUCCESS;
+
+        // Disconnect the slide loader if this is the last task
+        if (task.getId() == lastTask.getId()) {
+            slideLoader.DisConnect(retVal);
+        }
+        return status;
+	}
+	
+	public int waitForSlideLoader() {
+		int[] retVal = {0};
+        do {
+            try { Thread.sleep(1000); } catch (InterruptedException e) { }
+            slideLoader.get_Status(retVal);
+            //reportStatus(retVal[0], logger);
+        } while (((retVal[0] & getSTATE_STATEMASK()) != getSTATE_IDLE()) && !((retVal[0] & getLOADER_ERROR()) != 0));
+        return retVal[0];
 	}
 
     public void reportStatus(int status, Logger logger) {
@@ -189,10 +234,13 @@ public class SlideLoader implements Module {
                 poolSlides = poolSlideDao.select(where("poolId",new Integer(poolId.getValue())));
     		}
     	}
+    	Collections.sort(poolSlides);
+        poolSlides.add(null);
+
     	Dao<Task> taskDao = workflowRunner.getInstanceDb().table(Task.class);
     	List<Task> parentTasks = taskDao.select(where("moduleId",moduleId));
     	for (Task parentTask : parentTasks.size()>0? parentTasks.toArray(new Task[0]) : new Task[] {null}) {
-    		for (PoolSlide poolSlide : poolSlides.size()>0? poolSlides.toArray(new PoolSlide[0]) : new PoolSlide[] {null}) {
+    		for (PoolSlide poolSlide : poolSlides) {
                 Task task = new Task(moduleId, Status.NEW);
                 taskDao.insert(task);
                 task.createStorageLocation(
