d22a8aa161dcd406bc08d45ad8755a809a8c9bb8
Ben Booth <benwbooth@gmail.com>
2015-02-10 10:35:11 -0800

    remove old engine

--- MMSlideImage/src/org/bdgp/MMSlide/engine/EngineTask.java
+++
@@ -1,8 +0,0 @@
-package org.bdgp.MMSlide.engine;
-
-public interface EngineTask {
-   public void requestStop();
-   public void requestPause();
-   public void requestResume();
-   public void run();
-}
