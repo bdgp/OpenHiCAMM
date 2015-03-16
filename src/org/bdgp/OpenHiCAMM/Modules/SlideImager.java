e9104a5d833eabb2e6202f903f087a5d63bc9eac
Ben Booth <benwbooth@gmail.com>
2015-03-16 15:08:32 -0700

    remove unused imports

--- src/org/bdgp/OpenHiCAMM/Modules/SlideImager.java
+++ src/org/bdgp/OpenHiCAMM/Modules/SlideImager.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
