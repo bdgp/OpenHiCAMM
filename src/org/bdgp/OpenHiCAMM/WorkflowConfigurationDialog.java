40095bd6b8ef3af2922146b70d2e767786b3b920
insitu <insitu@fruitfly.org>
2015-03-25 09:57:12 -0700

    remove unused window-based config code

--- src/org/bdgp/OpenHiCAMM/WorkflowConfigurationDialog.java
+++ src/org/bdgp/OpenHiCAMM/WorkflowConfigurationDialog.java
@@ -3,7 +3,6 @@ package org.bdgp.OpenHiCAMM;
 import java.awt.Component;
 import java.awt.Dialog;
 import java.awt.Dimension;
-import java.awt.Window;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,7 +14,6 @@ import net.miginfocom.swing.MigLayout;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import org.bdgp.OpenHiCAMM.DB.Config;
@@ -24,11 +22,6 @@ import org.bdgp.OpenHiCAMM.Modules.Interfaces.Configuration;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
@@ -60,12 +53,7 @@ public class WorkflowConfigurationDialog extends JDialog {
             List<ModuleConfig> configs = config.select(where("id",entry.getKey()));
             Component panel = entry.getValue().display(configs.toArray(new Config[0]));
             if (panel != null) {
-            	if (Window.class.isAssignableFrom(panel.getClass())) {
-            		tabbedPane.add(entry.getKey(), new WindowedConfigPane((Window)panel));
-            	}
-            	else {
-                    tabbedPane.add(entry.getKey(), panel);
-            	}
+                tabbedPane.add(entry.getKey(), panel);
             }
         }
         getContentPane().add(tabbedPane, "cell 0 0 2 1,grow");
@@ -88,26 +76,6 @@ public class WorkflowConfigurationDialog extends JDialog {
             public void stateChanged(ChangeEvent e) {
                 btnPrevious.setEnabled(tabbedPane.getSelectedIndex() > 0);
                 btnNext.setEnabled(tabbedPane.getSelectedIndex() < tabbedPane.getTabCount()-1);
-                for (int i=0; i<tabbedPane.getTabCount(); ++i) {
-                    if (WindowedConfigPane.class.isAssignableFrom(tabbedPane.getComponentAt(i).getClass())) {
-                        final Window window = ((WindowedConfigPane)tabbedPane.getComponentAt(i)).getWindow();
-                        if (tabbedPane.getSelectedIndex() == i) {
-                        	window.setLocation(tabbedPane.getComponentAt(i).getLocationOnScreen());
-                        	window.setSize(tabbedPane.getComponentAt(i).getSize());
-                        	window.setVisible(true);
-                            java.awt.EventQueue.invokeLater(new Runnable() {
-                                @Override public void run() {
-                                    window.toFront();
-                                    window.repaint();
-                                    window.requestFocus();
-                                }
-                            });
-                        }
-                        else {
-                        	window.setVisible(false);
-                        }
-                    }
-                }
             }
         });
         
@@ -143,71 +111,6 @@ public class WorkflowConfigurationDialog extends JDialog {
             }
         });
         getContentPane().add(btnFinish, "cell 1 1");
-
-
-        this.addWindowListener(new WindowListener() {
-			@Override public void windowActivated(WindowEvent arg0) {
-                if (WindowedConfigPane.class.isAssignableFrom(tabbedPane.getSelectedComponent().getClass())) {
-                    final Window window = ((WindowedConfigPane)tabbedPane.getSelectedComponent()).getWindow();
-                    if (window.isVisible()) {
-                        window.setLocation(tabbedPane.getSelectedComponent().getLocationOnScreen());
-                        window.setSize(tabbedPane.getSelectedComponent().getSize());
-                        java.awt.EventQueue.invokeLater(new Runnable() {
-                            @Override public void run() {
-                                window.toFront();
-                                window.repaint();
-                            }
-                        });
-                    }
-                }
-			}
-			@Override public void windowClosed(WindowEvent arg0) { }
-			@Override public void windowClosing(WindowEvent arg0) { }
-			@Override public void windowDeactivated(WindowEvent arg0) { }
-			@Override public void windowDeiconified(WindowEvent arg0) { }
-			@Override public void windowIconified(WindowEvent arg0) { }
-			@Override public void windowOpened(WindowEvent arg0) {
-			}});
-        this.addComponentListener(new ComponentListener() {
-			@Override public void componentHidden(ComponentEvent arg0) { }
-			@Override public void componentMoved(ComponentEvent arg0) { 
-                if (WindowedConfigPane.class.isAssignableFrom(tabbedPane.getSelectedComponent().getClass())) {
-                    final Window window = ((WindowedConfigPane)tabbedPane.getSelectedComponent()).getWindow();
-                    window.setLocation(tabbedPane.getSelectedComponent().getLocationOnScreen());
-                    window.setSize(tabbedPane.getSelectedComponent().getSize());
-                    java.awt.EventQueue.invokeLater(new Runnable() {
-                        @Override public void run() {
-                            window.toFront();
-                            window.repaint();
-                        }
-                    });
-                }
-			}
-			@Override public void componentResized(ComponentEvent arg0) { 
-                if (WindowedConfigPane.class.isAssignableFrom(tabbedPane.getSelectedComponent().getClass())) {
-                    final Window window = ((WindowedConfigPane)tabbedPane.getSelectedComponent()).getWindow();
-                    window.setLocation(tabbedPane.getSelectedComponent().getLocationOnScreen());
-                    window.setSize(tabbedPane.getSelectedComponent().getSize());
-                    java.awt.EventQueue.invokeLater(new Runnable() {
-                        @Override public void run() {
-                            window.toFront();
-                            window.repaint();
-                        }
-                    });
-                }
-			}
-			@Override public void componentShown(ComponentEvent arg0) { }});
-
-        this.addWindowListener(new WindowAdapter() {
-        	@Override public void windowClosing(WindowEvent we) {
-                for (int i=0; i<tabbedPane.getTabCount(); ++i) {
-                    if (WindowedConfigPane.class.isAssignableFrom(tabbedPane.getComponentAt(i).getClass())) {
-                        Window window = ((WindowedConfigPane)tabbedPane.getComponentAt(i)).getWindow();
-                        window.setVisible(false);
-                    }
-                }
-        	}
-        });
     }
     
     public boolean validateConfiguration() {
@@ -222,7 +125,7 @@ public class WorkflowConfigurationDialog extends JDialog {
     		StringBuilder errorMessage = new StringBuilder("Please fix the following configuration errors:\n\n");
     		for (ValidationError error : errors) {
     			errorMessage.append(error.getMessage());
-    			errorMessage.append("\n\n");
+    			errorMessage.append(String.format("%n%n"));
     		}
     		JOptionPane.showMessageDialog(parent, errorMessage.toString(), "Configuration Errors", JOptionPane.ERROR_MESSAGE);
     		return false;
@@ -240,16 +143,5 @@ public class WorkflowConfigurationDialog extends JDialog {
                 }
             }
         }
-    }
-    
-    public static class WindowedConfigPane extends JPanel {
-    	Window window;
-    	public WindowedConfigPane(Window window) {
-    		super();
-    		this.window = window;
-    	}
-    	public Window getWindow() {
-    		return this.window;
-    	}
     }
 }
