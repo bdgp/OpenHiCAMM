3cccd6f688cefe2c43b4937abf48a41589d28677
insitu <insitu@fruitfly.org>
2015-08-26 15:56:31 -0700

    refresh the workflow dialog after the workflow runner dialog window is closed

--- src/org/bdgp/OpenHiCAMM/WorkflowDialog.java
+++ src/org/bdgp/OpenHiCAMM/WorkflowDialog.java
@@ -26,6 +26,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.File;
@@ -403,6 +404,11 @@ public class WorkflowDialog extends JDialog {
                 workflowRunnerDialog = new WorkflowRunnerDialog(WorkflowDialog.this, workflowRunner);
                 workflowRunnerDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 workflowRunnerDialog.pack();
+                workflowRunnerDialog.addWindowListener(new WindowAdapter() {
+                    @Override public void windowClosing(WindowEvent e) {
+                        WorkflowDialog.this.refresh();
+                    }
+                });
            }});
         }
     }
@@ -444,6 +450,11 @@ public class WorkflowDialog extends JDialog {
                     workflowRunnerDialog = new WorkflowRunnerDialog(WorkflowDialog.this, workflowRunner);
                     workflowRunnerDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                     workflowRunnerDialog.pack();
+                    workflowRunnerDialog.addWindowListener(new WindowAdapter() {
+                        @Override public void windowClosing(WindowEvent e) {
+                            WorkflowDialog.this.refresh();
+                        }
+                    });
                 }
                 workflowRunnerDialog.reset();
                 workflowRunnerDialog.setVisible(true);
