738d5d1e699be42230d262fb19176db7eace5358
insitu <insitu@fruitfly.org>
2015-09-01 17:07:08 -0700

    put the tile row,col in the grid coordinates instead of the tile stage positions

--- src/org/bdgp/OpenHiCAMM/Modules/ROIFinder.java
+++ src/org/bdgp/OpenHiCAMM/Modules/ROIFinder.java
@@ -170,8 +170,8 @@ public class ROIFinder implements Module, ImageLogger {
                         sp.stageName = "XYStage";
                         sp.x = x_stage-((tileX-(double)imageWidth/2.0)*pixelSizeUm);
                         sp.y = y_stage-((tileY-(double)imageHeight/2.0)*pixelSizeUm);
-                        msp.setGridCoordinates(tileY, tileX);
-                        String mspLabel = String.format("%s: ROI=%d, tileX=%d, tileY=%d", label, roi.getId(), tileX, tileY);
+                        msp.setGridCoordinates(y, x);
+                        String mspLabel = String.format("%s: ROI=%d, tileX=%d, tileY=%d", label, roi.getId(), x, y);
                         msp.setLabel(mspLabel);
                         msp.add(sp);
                         msp.setDefaultXYStage("XYStage");
