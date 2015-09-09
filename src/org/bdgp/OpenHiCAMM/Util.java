a437cd2616155ed722f1aa57a4460b879ebdee13
insitu <insitu@fruitfly.org>
2015-09-08 17:17:11 -0700

    add macroEscape() utility function for escaping strings within imagej macro parameters, make java 1.8 compatible

--- src/org/bdgp/OpenHiCAMM/Util.java
+++ src/org/bdgp/OpenHiCAMM/Util.java
@@ -3,6 +3,7 @@ package org.bdgp.OpenHiCAMM;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Util {
     /**
@@ -19,7 +20,6 @@ public class Util {
         public HashMap<K,V> with(K k, V v) { this.put(k, v); return this; }
         public HashMap<K,V> and(K k, V v) { this.put(k, v); return this; }
         public HashMap<K,V> o(K k, V v) { this.put(k, v); return this; }
-        public HashMap<K,V> _(K k, V v) { this.put(k, v); return this; }
         // in-place merge
         public Map<K,V> merge(Map<K,V> map) {
             this.putAll(map);
@@ -54,6 +54,7 @@ public class Util {
             this.addAll(l);
             return this;
         }
+        @SafeVarargs
         final public ArrayList<V> push(V ... values) {
             this.addAll(Arrays.asList(values));
             return this;
@@ -65,6 +66,7 @@ public class Util {
      * @param values The list of things to turn into a list literal.
      * @return
      */
+    @SafeVarargs
     public static <V> List<V> list(V ... values) {
         return Arrays.asList(values);
     }
@@ -113,4 +115,22 @@ public class Util {
     	if (str.matches("^[0-9]+$")) return str;
         return String.format("\"%s\"", str.replaceAll("(\b|\n|\t|\f|\r|\"|\\\\)", "\\\\$1"));
     }
+
+    public static String macroEscape(Object object) {
+        if (object == null) throw new RuntimeException("Cannot macroEscape null reference!");
+        String value = object.toString();
+
+        Pattern brackets = Pattern.compile("\\[|\\]");
+        Pattern singleQuote = Pattern.compile("'");
+        if (!brackets.matcher(value).find()) {
+            return String.format("[%s]", value);
+        }
+        else if (!singleQuote.matcher(value).find()) {
+            return String.format("'%s'", value);
+        }
+        else {
+            throw new RuntimeException(String.format("Could not macroEscape value: %s", value));
+        }
+    }
+
 }
