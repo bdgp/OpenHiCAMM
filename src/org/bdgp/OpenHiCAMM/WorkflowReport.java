8e5e7894b711fe79f91010f301d0b4ce179d43cf
Ben Booth <benwbooth@gmail.com>
2015-10-02 12:44:28 -0700

    finish inital draft of slide overview

--- src/org/bdgp/OpenHiCAMM/WorkflowReport.java
+++ src/org/bdgp/OpenHiCAMM/WorkflowReport.java
@@ -1,29 +1,39 @@
 package org.bdgp.OpenHiCAMM;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.awt.Color;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 import org.bdgp.OpenHiCAMM.DB.Acquisition;
 import org.bdgp.OpenHiCAMM.DB.Config;
 import org.bdgp.OpenHiCAMM.DB.Image;
+import org.bdgp.OpenHiCAMM.DB.PoolSlide;
+import org.bdgp.OpenHiCAMM.DB.ROI;
+import org.bdgp.OpenHiCAMM.DB.Slide;
 import org.bdgp.OpenHiCAMM.DB.Task;
+import org.bdgp.OpenHiCAMM.DB.TaskConfig;
 import org.bdgp.OpenHiCAMM.DB.WorkflowModule;
-import org.bdgp.OpenHiCAMM.Modules.ROIFinder;
 import org.bdgp.OpenHiCAMM.Modules.SlideImager;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.micromanager.api.MultiStagePosition;
 import org.micromanager.api.PositionList;
-import org.micromanager.utils.MMException;
+import org.micromanager.utils.MMSerializationException;
+
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 import ij.ImagePlus;
 import ij.gui.NewImage;
+import ij.gui.Roi;
 import ij.process.Blitter;
 import ij.process.ImageProcessor;
 import javafx.application.Platform;
@@ -34,6 +44,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import jdk.nashorn.api.scripting.JSObject;
 
 import static org.bdgp.OpenHiCAMM.Util.where;
 import static org.bdgp.OpenHiCAMM.Tag.*;
@@ -41,12 +52,16 @@ import static org.bdgp.OpenHiCAMM.Tag.*;
 @SuppressWarnings("serial")
 public class WorkflowReport extends JFrame {
 	WorkflowRunner workflowRunner;
+
+	// TODO: convert to configurable values
     public static final int SLIDE_PREVIEW_WIDTH = 1280; 
+    public static final boolean INVERT_X = true; 
+    public static final boolean INVERT_Y = true; 
 
 	public WorkflowReport(WorkflowRunner workflowRunner) {
 		this.workflowRunner = workflowRunner;
 
-		final JFXPanel fxPanel = new JFXPanel();
+		JFXPanel fxPanel = new JFXPanel();
         this.add(fxPanel);
         this.setSize(1024, 768);
         this.setVisible(true);
@@ -62,8 +77,10 @@ public class WorkflowReport extends JFrame {
 	public void initFX(JFXPanel fxPanel) {
         Scene scene = new Scene(new Group());
         VBox root = new VBox();     
-        final WebView browser = new WebView();
-        final WebEngine webEngine = browser.getEngine();
+        WebView browser = new WebView();
+        WebEngine webEngine = browser.getEngine();
+        JSObject jsobj = (JSObject) webEngine.executeScript("window");
+        jsobj.setMember("workflowReport", this);
 
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setContent(browser);
@@ -90,23 +107,37 @@ public class WorkflowReport extends JFrame {
 		}).toString();
 	}
 
+    Double minX = null;
+    Double minY = null;
+    Double maxX = null;
+    Double maxY = null;
+    Double maxOverlapUmX = null;
+    Double maxOverlapUmY = null;
+    Double maxOverlapPixelsX = null;
+    Double maxOverlapPixelsY = null;
+    Integer maxGridCol = null;
+    Integer maxGridRow = null;
+
 	private void runReport(String startModuleId) {
 	    WorkflowModule startModule = this.workflowRunner.getWorkflow().selectOneOrDie(where("id", startModuleId));
 
 	    // get the loader module
+	    // get the ROIFinder module(s)
 	    WorkflowModule loaderModule = null;
+	    List<WorkflowModule> roiFinderModules = new ArrayList<WorkflowModule>();
 	    if (startModule.getParentId() != null) {
-	        WorkflowModule loader = this.workflowRunner.getWorkflow().selectOneOrDie(where("id", startModule.getParentId()));
+	        WorkflowModule wm = this.workflowRunner.getWorkflow().selectOneOrDie(where("id", startModule.getParentId()));
 	        if (this.workflowRunner.getModuleConfig().selectOne(
-	                where("id", loader.getId()).and("key", "canLoadSlides").and("value", "yes")) != null) 
+	                where("id", wm.getId()).and("key", "canLoadSlides").and("value", "yes")) != null) 
 	        {
-	            loaderModule = loader;
+	            loaderModule = wm;
+	        }
+	        if (this.workflowRunner.getModuleConfig().selectOne(
+	                where("id", wm.getId()).and("key", "canProduceROIs").and("value", "yes")) != null) 
+	        {
+	            roiFinderModules.add(wm);
 	        }
 	    }
-	    
-	    // get the ROIFinder module(s)
-	    List<WorkflowModule> roiFinderModules = this.workflowRunner.getWorkflow().select(
-	            where("parentId", startModule.getId()).and("moduleName", ROIFinder.class.getName()));
 
 	    // get the associated hi res ROI slide imager modules for each ROI finder module
 	    Map<WorkflowModule,List<WorkflowModule>> roiImagers = new HashMap<WorkflowModule,List<WorkflowModule>>();
@@ -124,31 +155,62 @@ public class WorkflowReport extends JFrame {
             }
 	    }
 	    
-	    // get the position list
-        Config posListConf = this.workflowRunner.getModuleConfig().selectOneOrDie(where("id",startModuleId).and("key","posListFile"));
-        File posListFile = new File(posListConf.getValue());
-        if (!posListFile.exists()) {
-            throw new RuntimeException("Cannot find position list file "+posListFile.getPath());
-        }
-        PositionList positionList = null;
-        try { 
-            positionList = new PositionList();
-            positionList.load(posListFile.getPath()); 
-        } 
-        catch (MMException e) {throw new RuntimeException(e);}
-        
+	    // display the title
+        Dao<PoolSlide> psDao = this.workflowRunner.getInstanceDb().table(PoolSlide.class);
+        Dao<Slide> slideDao = this.workflowRunner.getInstanceDb().table(Slide.class);
+        Map<String,Config> loaderConfig = new HashMap<String,Config>();
+        List<PoolSlide> ps = new ArrayList<PoolSlide>();
+        Slide slide;
+        String slideId;
+	    if (loaderModule != null) {
+	    	List<TaskConfig> loaderConfigs = this.workflowRunner.getTaskConfig().select(where("id", loaderModule.getId()));
+	    	for (Config conf : loaderConfigs) {
+	    		loaderConfig.put(conf.getKey(), conf);
+	    	}
+	    	if (loaderConfig.containsKey("slideId")) {
+	    		slide = slideDao.selectOneOrDie(where("id", loaderConfig.get("slideId")));
+	    		ps.addAll(psDao.select(where("slideId", slide.getId())));
+	    		slideId = loaderConfig.get("slideId").getValue();
+	    	}
+	    	else {
+	    		slide = null;
+	    		slideId = "slide";
+	    	}
+
+            H1().with(()->text(
+            		String.format("SlideImager %s%s", 
+            				startModuleId, 
+            				loaderConfig.containsKey("slideId")? 
+            						String.format(", Slide %s%s", 
+            								slide.getName(),
+            								!ps.isEmpty()? 
+            										ps.stream().map(p->String.format(", Pool %d, Cartridge %d, Slide Position %d", 
+            												p.getPoolId(), p.getCartridgePosition(), p.getSlidePosition())).
+            										collect(Collectors.joining("; ")) 
+            										: "") 
+            						: "")));
+	    }
+	    else {
+            H1().with(()->text(String.format("SlideImager %s", startModuleId)));
+            slide = null;
+            slideId = "slide";
+	    }
+
         // compute the width and height of the imaging grid in pixels from the position list
-        Double minX = null;
-        Double minY = null;
-        Double maxX = null;
-        Double maxY = null;
-        Double maxOverlapUmX = null;
-        Double maxOverlapUmY = null;
-        Double maxOverlapPixelsX = null;
-        Double maxOverlapPixelsY = null;
-        Integer maxGridCol = null;
-        Integer maxGridRow = null;
-        for (MultiStagePosition msp : positionList.getPositions()) {
+        minX = null;
+        minY = null;
+        maxX = null;
+        maxY = null;
+        maxOverlapUmX = null;
+        maxOverlapUmY = null;
+        maxOverlapPixelsX = null;
+        maxOverlapPixelsY = null;
+        maxGridCol = null;
+        maxGridRow = null;
+        List<Task> imageTasks = this.workflowRunner.getTaskStatus().select(where("moduleId", startModuleId));
+        for (Task task : imageTasks) {
+        	MultiStagePosition msp = getMsp(task);
+
             if (minX == null || msp.getX() < minX) minX = msp.getX();
             if (maxX == null || msp.getX() > maxX) maxX = msp.getX();
             if (minY == null || msp.getY() < minY) minY = msp.getY();
@@ -191,24 +253,88 @@ public class WorkflowReport extends JFrame {
         
         Dao<Image> imageDao = this.workflowRunner.getInstanceDb().table(Image.class);
         Dao<Acquisition> acqDao = this.workflowRunner.getInstanceDb().table(Acquisition.class);
+        Dao<ROI> roiDao = this.workflowRunner.getInstanceDb().table(ROI.class);
         ImagePlus slideThumb = NewImage.createRGBImage("slideThumb", SLIDE_PREVIEW_WIDTH, slidePreviewHeight, 1, NewImage.FILL_WHITE);
         ImageProcessor slideThumbIp = slideThumb.getProcessor();
-        List<Task> imageTasks = this.workflowRunner.getTaskStatus().select(where("moduleId", startModuleId));
-        for (Task task : imageTasks) {
-            Config imageIdConf = this.workflowRunner.getTaskConfig().selectOne(
-                    where("id", new Integer(task.getId()).toString()).and("key", "imageId"));
-            if (imageIdConf != null) {
-                // Get a thumbnail of the image
-                Image image = imageDao.selectOneOrDie(where("id", new Integer(imageIdConf.getValue())));
-                ImagePlus imp = image.getImagePlus(acqDao);
-                ImageProcessor ip = imp.getProcessor();
-                ip.setInterpolate(true);
-                imp.setProcessor(imp.getTitle(), ip.resize(
-                        (int)Math.floor(ip.getWidth() * scaleFactor), 
-                        (int)Math.floor(ip.getHeight() * scaleFactor)));
-                // TODO: compute xloc, yloc
-                //slideThumbIp.copyBits(ip, xloc, yloc, Blitter.COPY);
+        Map(name->String.format("map-%s", slideId)).with(()->{
+            for (Task task : imageTasks) {
+                Config imageIdConf = this.workflowRunner.getTaskConfig().selectOne(
+                        where("id", new Integer(task.getId()).toString()).and("key", "imageId"));
+                if (imageIdConf != null) {
+                    MultiStagePosition msp = getMsp(task);
+
+                    // Get a thumbnail of the image
+                    Image image = imageDao.selectOneOrDie(where("id", new Integer(imageIdConf.getValue())));
+                    ImagePlus imp = image.getImagePlus(acqDao);
+                    ImageProcessor ip = imp.getProcessor();
+                    ip.setInterpolate(true);
+                    imp.setProcessor(imp.getTitle(), ip.resize(
+                            (int)Math.floor(ip.getWidth() * scaleFactor), 
+                            (int)Math.floor(ip.getHeight() * scaleFactor)));
+                    
+                    int xloc1 = (int)Math.floor(((msp.getX() - minX) / pixelSizeX) - (imp.getWidth() / 2.0));
+                    int xloc = INVERT_X? gridWidthPx - (xloc1 + imp.getWidth()) : xloc1;
+                    int yloc1 = (int)Math.floor(((msp.getY() - minY) / pixelSizeY) - (imp.getHeight() / 2.0));
+                    int yloc = INVERT_Y? yloc = gridHeightPx - (yloc1 + imp.getHeight()) : yloc1;
+                    slideThumbIp.copyBits(ip, xloc, yloc, Blitter.COPY);
+                    slideThumbIp.setColor(new Color(0, 0, 0));
+                    slideThumbIp.drawRect(xloc, yloc, ip.getWidth(), ip.getHeight());
+                    
+                    // write the ROI areas first so they take precedence
+                    for (ROI roi : roiDao.select(where("imageId", image.getId()))) {
+                    	int roiX = (int)Math.floor(xloc + (roi.getX1() * scaleFactor));
+                    	int roiY = (int)Math.floor(yloc + (roi.getY1() * scaleFactor));
+                    	int roiWidth = (int)Math.floor((roi.getX2()-roi.getX1()+1) * scaleFactor);
+                        int roiHeight = (int)Math.floor((roi.getY2()-roi.getY1()+1) * scaleFactor);
+                        slideThumbIp.setColor(new Color(255, 0, 0));
+                    	slideThumbIp.drawRect(roiX, roiY, roiWidth, roiHeight);
+                    	
+                    	Area(shape->"rect",
+                    			coords->String.format("%d,%d,%d,%d", roiX, roiY, roiX+roiWidth, roiY+roiHeight),
+                    			href->String.format("#area-ROI-%s", roi.getId()),
+                    			title->roi.toString());
+                    }
+                    
+                    Area(shape->"rect", 
+                    		coords->String.format("%d,%d,%d,%d", xloc, yloc, xloc+imp.getWidth(), yloc+imp.getHeight()),
+                    		title->image.getName(),
+                    		onClick->String.format("workflowReport.showImage(%d)", image.getId()));
+                }
             }
-        }
+        });
+
+        // write the slide thumbnail as an embedded HTML image.
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try { ImageIO.write(slideThumb.getBufferedImage(), "jpg", baos); } 
+        catch (IOException e) {throw new RuntimeException(e);}
+        Img(src->String.format("data:image/jpeg;base64,%s", Base64.encode(baos.toByteArray())),
+        		width->slideThumb.getWidth(),
+        		height->slideThumb.getHeight(),
+        		usemap->String.format("#map-%s", slideId));
+	}
+	
+	public void showImage(int imageId) {
+        // Get a thumbnail of the image
+        Dao<Image> imageDao = this.workflowRunner.getInstanceDb().table(Image.class);
+        Dao<Acquisition> acqDao = this.workflowRunner.getInstanceDb().table(Acquisition.class);
+        Image image = imageDao.selectOneOrDie(where("id", imageId));
+        ImagePlus imp = image.getImagePlus(acqDao);
+        imp.show();
+	}
+	
+	private MultiStagePosition getMsp(Task task) {
+        // get the MSP from the task config
+        PositionList posList = new PositionList();
+        try {
+            Config mspConf = this.workflowRunner.getTaskConfig().selectOneOrDie(where("id", task.getId()).and("key","MSP"));
+            JSONObject posListJson = new JSONObject().
+                    put("POSITIONS", new JSONArray().put(new JSONObject(mspConf.getValue()))).
+                    put("VERSION", 3).
+                    put("ID","Micro-Manager XY-position list");
+            posList.restore(posListJson.toString());
+        } 
+        catch (JSONException | MMSerializationException e) {throw new RuntimeException(e);}
+        MultiStagePosition msp = posList.getPosition(0);
+        return msp;
 	}
 }
