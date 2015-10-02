21ff8ab42242703b431ad4d9f2bad7ab4a4c0cf1
insitu <insitu@fruitfly.org>
2015-10-01 17:39:00 -0700

    keep working on imaging reports, still in progress...

--- imaging-report.pl
+++ imaging-report.pl
@@ -4,6 +4,7 @@ use warnings;
 use FileHandle;
 use MIME::Base64;
 use File::Slurp qw(slurp);
+use POSIX qw(ceil);
 
 use JSON;
 use CGI;
@@ -11,16 +12,17 @@ use Imager;
 use DBI;
 use DBD::ODBC;
 
-my $pixelsize = 0.48;
-my $hirespixelsize = 0.1253;
+my $dsn = [slurp([glob('*.lock')]->[0], chomp=>1)]->[0] or die "Could not read DSN";
+my ($user, $pass) = slurp([glob('*.login')]->[0], chomp=>1) or die "Could not read login info";
+my $dbh = DBI->connect("dbi:ODBC:DSN=$dsn", $user, $pass)
+  or die "Could not open DB handle $dsn: ".DBI->errstr;
+$dbh->{RaiseError} = 1;
 
 my $cgi = CGI->new;
-
-my $slide = 1;
-
 print $cgi->start_html;
 print $cgi->start_body;
 
+my $slide = 1;
 while (@ARGV) {
   my $dir1=shift(@ARGV);
   my $dir2=shift(@ARGV) or next;
@@ -34,7 +36,7 @@ while (@ARGV) {
       col=>[/^1-Pos_([0-9]+)_[0-9]+$/]->[0],
       row=>[/^1-Pos_[0-9]+_([0-9]+)$/]->[0],
       imagefile=>[glob("$dir1/$posname/*.tif")]->[0],
-      metadata=>JSON::from_json(slurp([glob("$dir1/$posname/metadata.txt")]->[0])),
+      metadata=>JSON::from_json([slurp([glob("$dir1/$posname/metadata.txt")]->[0], chomp=>1)]->[0]),
     );
     $pos[$posname{row}][$posname{col}] = \%posname;
   }
@@ -54,7 +56,7 @@ while (@ARGV) {
       tilex=>$roitile=~[/tileX=([0-9]+)/]->[0],
       tiley=>$roitile=~[/tileY=([0-9]+)/]->[0],
       imagefiles=>[sort glob("$dir2/$roitile/*.tif")],
-      metadata=>JSON::from_json(slurp([glob("$dir2/$roitile/metadata.txt")]->[0])),
+      metadata=>JSON::from_json([slurp([glob("$dir2/$roitile/metadata.txt")]->[0], chomp=>1)]->[0]),
     );
     %{$pos[$roitile{row}][$roitile{col}]} = (
       %{$pos[$roitile{row}][$roitile{col}]||{}},
@@ -78,10 +80,44 @@ while (@ARGV) {
       print $cgi->start_td;
       print $cgi->dl(map {($cgi->dt($_), $cgi->dd($pos->{$_}))} sort keys %$pos);
       my $img = Imager->new($pos->{imagefile}) or die Imager->errstr;
-      for my $roi (@{$pos->{rois}||[]}) {
+      for my $roi_id (sort keys %{$pos->{rois}||[]}) {
+        my $roi = $pos->{rois}{$roi_id};
         # convert each tile stage position into x,y,w,h box coordinates of the
         # low-res image
-        
+
+        my $pixelSizeUm = 0.48;
+        my $hiResPixelSizeUm = 0.1253;
+        my $cameraWidth = 4928;
+        my $cameraHeight = 3264;
+        my $overlapPct = 25;
+
+        my $r = query($dbh, "select * from roi where \"id\"=?", $roi_id);
+        my ($roiX1, $roiX2, $roiY1, $roiY2) = ($r->{x1}, $r->{x2}, $r->{y1}, $r->{y2});
+
+        my $roiWidth = $roiX2-$roiX1+1;
+        my $roiHeight = $roiY2-$roiY1+1;
+        my $tileWidth = int(($cameraWidth * $hiResPixelSizeUm) / $pixelSizeUm);
+        my $tileHeight = int(($cameraHeight * $hiResPixelSizeUm) / $pixelSizeUm);
+        my $tileXOverlap = int(($overlapPct / 100.0) * $tileWidth);
+        my $tileYOverlap = int(($overlapPct / 100.0) * $tileHeight);
+        my $tileXCount = ceil(($roiWidth - $tileXOverlap) / ($tileWidth - $tileXOverlap));
+        my $tileYCount = ceil(($roiHeight - $tileYOverlap) / ($tileHeight - $tileYOverlap));
+        my $tileSetWidth = ($tileXCount * ($tileWidth - $tileXOverlap)) + $tileXOverlap;
+        my $tileSetHeight = ($tileYCount * ($tileHeight - $tileYOverlap)) + $tileYOverlap;
+        my $tileXOffset = int(($roiX1 + ($roiWidth / 2.0)) - ($tileSetWidth / 2.0) + ($tileWidth / 2.0));
+        my $tileYOffset = int(($roiY1 + ($roiHeight / 2.0)) - ($tileSetHeight / 2.0) + ($tileHeight / 2.0));
+
+        for (my $x=0; $x < $tileXCount; ++$x) {
+          for (my $y=0; $y < $tileYCount; ++$y) {
+            my $tileX = ($x*($tileWidth - $tileXOverlap)) + $tileXOffset;
+            my $tileY = ($y*($tileHeight - $tileYOverlap)) + $tileYOffset;
+
+            $img->box(
+              color=>Imager::Color->new(0, 0, 0), 
+              x1=>$tileX, y1=>$tileY, x2=>$tileX+$tileWidth, y2=>$tileY+$tileHeight, aa=>1, endp=>1);
+
+          }
+        }
       }
       print $cgi->end_td;
     }
@@ -97,3 +133,16 @@ print $cgi->end_body;
 print $cgi->end_html;
 
 exit 0;
+
+sub query {
+  my $dbh = shift;
+  my $sql = shift;
+  my $sth = $dbh->prepare($sql)
+    or die "Could not prepare statement: $sql";
+  $sth->execute(@_);
+  my @ret;
+  while (my $r = $sth->fetchrow_hashref) {
+    push @ret, $r;
+  }
+  return @ret;
+}
--- src/org/bdgp/OpenHiCAMM/WorkflowReport.java
+++ src/org/bdgp/OpenHiCAMM/WorkflowReport.java
@@ -1,12 +1,31 @@
 package org.bdgp.OpenHiCAMM;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JFrame;
 
-import j2html.tags.Tag;
+import org.bdgp.OpenHiCAMM.DB.Acquisition;
+import org.bdgp.OpenHiCAMM.DB.Config;
+import org.bdgp.OpenHiCAMM.DB.Image;
+import org.bdgp.OpenHiCAMM.DB.Task;
+import org.bdgp.OpenHiCAMM.DB.WorkflowModule;
+import org.bdgp.OpenHiCAMM.Modules.ROIFinder;
+import org.bdgp.OpenHiCAMM.Modules.SlideImager;
+import org.micromanager.api.MultiStagePosition;
+import org.micromanager.api.PositionList;
+import org.micromanager.utils.MMException;
+
+import ij.ImagePlus;
+import ij.gui.NewImage;
+import ij.process.Blitter;
+import ij.process.ImageProcessor;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Group;
@@ -16,12 +35,13 @@ import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-import static j2html.TagCreator.*;
+import static org.bdgp.OpenHiCAMM.Util.where;
+import static org.bdgp.OpenHiCAMM.Tag.*;
 
 @SuppressWarnings("serial")
 public class WorkflowReport extends JFrame {
 	WorkflowRunner workflowRunner;
-	public static final boolean DEBUG_MODE = true;
+    public static final int SLIDE_PREVIEW_WIDTH = 1280; 
 
 	public WorkflowReport(WorkflowRunner workflowRunner) {
 		this.workflowRunner = workflowRunner;
@@ -56,21 +76,139 @@ public class WorkflowReport extends JFrame {
 	}
 	
 	public String runReport() {
-		Tag html = html().with(
-			body().with(
-					
-            )
-        );
-		
-		
-		if (DEBUG_MODE) {
-			try {
-				PrintWriter pw = new PrintWriter("workflow_report.html");
-				pw.println(html.render());
-				pw.close();
-			} 
-			catch (FileNotFoundException e) {throw new RuntimeException(e);}
-		}
-		return html.render();
+	    // Find SlideImager modules where there is no associated posListModuleId module config
+	    // This is the starting SlideImager module.
+		return Html().with(()->{
+			Body().with(()->{
+                List<WorkflowModule> slideImagers = this.workflowRunner.getWorkflow().select(where("moduleName", SlideImager.class.getName()));
+                for (WorkflowModule slideImager : slideImagers) {
+                    if (!workflowRunner.getTaskConfig().select(where("moduleId", slideImager.getId()).and("key", "posListFile")).isEmpty()) {
+                        runReport(slideImager.getId());
+                    }
+                }
+			});
+		}).toString();
+	}
+
+	private void runReport(String startModuleId) {
+	    WorkflowModule startModule = this.workflowRunner.getWorkflow().selectOneOrDie(where("id", startModuleId));
+
+	    // get the loader module
+	    WorkflowModule loaderModule = null;
+	    if (startModule.getParentId() != null) {
+	        WorkflowModule loader = this.workflowRunner.getWorkflow().selectOneOrDie(where("id", startModule.getParentId()));
+	        if (this.workflowRunner.getModuleConfig().selectOne(
+	                where("id", loader.getId()).and("key", "canLoadSlides").and("value", "yes")) != null) 
+	        {
+	            loaderModule = loader;
+	        }
+	    }
+	    
+	    // get the ROIFinder module(s)
+	    List<WorkflowModule> roiFinderModules = this.workflowRunner.getWorkflow().select(
+	            where("parentId", startModule.getId()).and("moduleName", ROIFinder.class.getName()));
+
+	    // get the associated hi res ROI slide imager modules for each ROI finder module
+	    Map<WorkflowModule,List<WorkflowModule>> roiImagers = new HashMap<WorkflowModule,List<WorkflowModule>>();
+	    for (WorkflowModule roiFinderModule : roiFinderModules) {
+            List<WorkflowModule> slideImagers = this.workflowRunner.getWorkflow().select(where("moduleName", SlideImager.class.getName()));
+            for (WorkflowModule slideImager : slideImagers) {
+                if (!workflowRunner.getTaskConfig().select(
+                        where("moduleId", slideImager.getId()).and("key", "posListModuleId").and("value", roiFinderModule.getId())).isEmpty()) 
+                {
+                    if (!roiImagers.containsKey(roiFinderModule)) {
+                        roiImagers.put(roiFinderModule, new ArrayList<WorkflowModule>());
+                    }
+                    roiImagers.get(roiFinderModule).add(slideImager);
+                }
+            }
+	    }
+	    
+	    // get the position list
+        Config posListConf = this.workflowRunner.getModuleConfig().selectOneOrDie(where("id",startModuleId).and("key","posListFile"));
+        File posListFile = new File(posListConf.getValue());
+        if (!posListFile.exists()) {
+            throw new RuntimeException("Cannot find position list file "+posListFile.getPath());
+        }
+        PositionList positionList = null;
+        try { 
+            positionList = new PositionList();
+            positionList.load(posListFile.getPath()); 
+        } 
+        catch (MMException e) {throw new RuntimeException(e);}
+        
+        // compute the width and height of the imaging grid in pixels from the position list
+        Double minX = null;
+        Double minY = null;
+        Double maxX = null;
+        Double maxY = null;
+        Double maxOverlapUmX = null;
+        Double maxOverlapUmY = null;
+        Double maxOverlapPixelsX = null;
+        Double maxOverlapPixelsY = null;
+        Integer maxGridCol = null;
+        Integer maxGridRow = null;
+        for (MultiStagePosition msp : positionList.getPositions()) {
+            if (minX == null || msp.getX() < minX) minX = msp.getX();
+            if (maxX == null || msp.getX() > maxX) maxX = msp.getX();
+            if (minY == null || msp.getY() < minY) minY = msp.getY();
+            if (maxY == null || msp.getY() > maxY) maxY = msp.getY();
+            if (maxGridCol == null || msp.getGridColumn()+1 > maxGridCol) maxGridCol = msp.getGridColumn()+1;
+            if (maxGridRow == null || msp.getGridRow()+1 > maxGridRow) maxGridRow = msp.getGridRow()+1;
+            if (msp.getProperty("OverlapUmX") != null && 
+                    (maxOverlapUmX == null || new Double(msp.getProperty("OverlapUmX")) > maxOverlapUmX)) 
+            {
+                maxOverlapUmX = new Double(msp.getProperty("OverlapUmX"));
+            }
+            if (msp.getProperty("OverlapUmY") != null && 
+                    (maxOverlapUmY == null || new Double(msp.getProperty("OverlapUmY")) > maxOverlapUmY)) 
+            {
+                maxOverlapUmY = new Double(msp.getProperty("OverlapUmY"));
+            }
+            if (msp.getProperty("OverlapPixelsX") != null && 
+                    (maxOverlapPixelsX == null || new Double(msp.getProperty("OverlapPixelsX")) > maxOverlapPixelsX)) 
+            {
+                maxOverlapPixelsX = new Double(msp.getProperty("OverlapPixelsX"));
+            }
+            if (msp.getProperty("OverlapPixelsY") != null && 
+                    (maxOverlapPixelsY == null || new Double(msp.getProperty("OverlapPixelsY")) > maxOverlapPixelsY)) 
+            {
+                maxOverlapPixelsY = new Double(msp.getProperty("OverlapPixelsY"));
+            }
+        }
+        double cellWidthUm = ((maxX - minX) + (maxOverlapUmX * (maxGridCol-1))) / (maxGridCol-1);
+        double cellHeightUm = ((maxY - minY) + (maxOverlapUmY * (maxGridRow-1))) / (maxGridRow-1);
+        double gridWidthUm = (maxX - minX) + cellWidthUm;
+        double gridHeightUm =  (maxY - minY) + cellHeightUm;
+        double pixelSizeX = maxOverlapUmX / maxOverlapPixelsX;
+        double pixelSizeY = maxOverlapUmY / maxOverlapPixelsY;
+        int gridWidthPx = (int)Math.floor(gridWidthUm / pixelSizeX);
+        int gridHeightPx = (int)Math.floor(gridHeightUm / pixelSizeY);
+        
+        // this is the scale factor for creating the thumbnail images
+        double scaleFactor = SLIDE_PREVIEW_WIDTH / gridWidthPx;
+        int slidePreviewHeight = (int)Math.floor(scaleFactor * gridHeightPx);
+        
+        Dao<Image> imageDao = this.workflowRunner.getInstanceDb().table(Image.class);
+        Dao<Acquisition> acqDao = this.workflowRunner.getInstanceDb().table(Acquisition.class);
+        ImagePlus slideThumb = NewImage.createRGBImage("slideThumb", SLIDE_PREVIEW_WIDTH, slidePreviewHeight, 1, NewImage.FILL_WHITE);
+        ImageProcessor slideThumbIp = slideThumb.getProcessor();
+        List<Task> imageTasks = this.workflowRunner.getTaskStatus().select(where("moduleId", startModuleId));
+        for (Task task : imageTasks) {
+            Config imageIdConf = this.workflowRunner.getTaskConfig().selectOne(
+                    where("id", new Integer(task.getId()).toString()).and("key", "imageId"));
+            if (imageIdConf != null) {
+                // Get a thumbnail of the image
+                Image image = imageDao.selectOneOrDie(where("id", new Integer(imageIdConf.getValue())));
+                ImagePlus imp = image.getImagePlus(acqDao);
+                ImageProcessor ip = imp.getProcessor();
+                ip.setInterpolate(true);
+                imp.setProcessor(imp.getTitle(), ip.resize(
+                        (int)Math.floor(ip.getWidth() * scaleFactor), 
+                        (int)Math.floor(ip.getHeight() * scaleFactor)));
+                // TODO: compute xloc, yloc
+                //slideThumbIp.copyBits(ip, xloc, yloc, Blitter.COPY);
+            }
+        }
 	}
 }
