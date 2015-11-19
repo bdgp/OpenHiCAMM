ee91ae82446de9f9705d089851db1acb9b03cc7a
insitu <insitu@fruitfly.org>
2015-11-18 17:40:05 -0800

    fix rlwrap parameters in sqltool script

--- sqltool.sh
+++ sqltool.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #export MAVEN_OPTS='-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=4000' 
 if hash rlwrap &>/dev/null; then
-  RLWRAP=rlwrap
+  RLWRAP='rlwrap -z pipeto'
 fi
-exec $RLWRAP -z pipeto mvn -f "$(dirname "$0")/pom.xml" -q exec:java -Dexec.mainClass=org.bdgp.OpenHiCAMM.SqlTool -Dexec.classpathScope=compile -Dexec.args="$*"
+exec $RLWRAP mvn -f "$(dirname "$0")/pom.xml" -q exec:java -Dexec.mainClass=org.bdgp.OpenHiCAMM.SqlTool -Dexec.classpathScope=compile -Dexec.args="$*"
