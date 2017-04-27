5ca4f4e3911017d721321a04a128ae503c3d7ae4
insitu <insitu@fruitfly.org>
2017-04-27 14:31:10 -0700

    use number of processors for pool thread count

--- src/org/bdgp/OpenHiCAMM/WorkflowReport.java
+++ src/org/bdgp/OpenHiCAMM/WorkflowReport.java
@@ -245,7 +245,7 @@ public class WorkflowReport implements Report {
             });
         });
 
-        ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()*2);
+        ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         List<Future<?>> futures = new ArrayList<>();
         for (Map.Entry<String,Runnable> entry : runnables.entrySet()) {
             String reportFile = entry.getKey();
