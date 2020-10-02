4e6091995fd800504e31d0d0d23a3c5b96372cf7
insitu <insitu@fruitfly.org>
2020-10-02 12:25:51 -0700

    add mmplugin wrapper

---
+++ src/org/bdgp/OpenHiCAMM/mmplugin/OpenHiCAMMPlugin.java
@@ -0,0 +1,54 @@
+package org.bdgp.OpenHiCAMM.mmplugin;
+
+import org.bdgp.OpenHiCAMM.OpenHiCAMM;
+import org.micromanager.MenuPlugin;
+import org.micromanager.Studio;
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.SciJavaPlugin;
+
+@Plugin(type = MenuPlugin.class)
+public class OpenHiCAMMPlugin implements MenuPlugin, SciJavaPlugin {
+	OpenHiCAMM openhicamm;
+	
+	public OpenHiCAMMPlugin() {
+		if (this.openhicamm == null) {
+			this.openhicamm = new OpenHiCAMM();
+		}
+	}
+
+	@Override
+	public void setContext(Studio studio) {
+		this.openhicamm.setContext(studio);
+	}
+
+	@Override
+	public String getName() {
+		return this.openhicamm.getName();
+	}
+
+	@Override
+	public String getHelpText() {
+		return this.openhicamm.getHelpText();
+	}
+
+	@Override
+	public String getVersion() {
+		return this.openhicamm.getVersion();
+	}
+
+	@Override
+	public String getCopyright() {
+		return this.openhicamm.getCopyright();
+	}
+
+	@Override
+	public String getSubMenu() {
+		return this.openhicamm.getSubMenu();
+	}
+
+	@Override
+	public void onPluginSelected() {
+		this.openhicamm.onPluginSelected();
+	}
+
+}
