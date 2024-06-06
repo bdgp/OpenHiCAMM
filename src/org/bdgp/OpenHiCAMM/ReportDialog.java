f3c9f8b2ae8f65f2454e7e6979d2f9552fa58cfa
insitu <insitu@fruitfly.org>
2024-06-06 12:16:41 -0700

    fix import

--- src/org/bdgp/OpenHiCAMM/ReportDialog.java
+++ src/org/bdgp/OpenHiCAMM/ReportDialog.java
@@ -37,7 +37,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebErrorEvent;
 import javafx.scene.web.WebView;
-import jdk.nashorn.api.scripting.JSObject;
+import netscape.javascript.JSObject;
 
 import static org.bdgp.OpenHiCAMM.Util.where;
 
